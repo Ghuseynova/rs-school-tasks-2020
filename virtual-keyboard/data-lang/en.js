ae4337f44e81ef05a8bc58b67c14ee4ab5e8ec7c
Gunel Huseynova <gorellik246@gmail.com>
2020-11-05 00:10:12 +0300

    feat: add key data

---
+++ virtual-keyboard/data-lang/en.js
@@ -0,0 +1,332 @@
+const en = [
+	{
+		symbol: '`',
+		symbolSm: 'Ё',
+		shift: '~',
+	},
+	{
+		symbol: '1',
+		shift: '!',
+	},
+	{
+		symbol: '2',
+		symbolSm: '"',
+		shift: '@',
+	},
+	{
+		symbol: '3',
+		symbolSm: '№',
+		shift: '#',
+	},
+	{
+		symbol: '4',
+		symbolSm: ';',
+		shift: '$',
+	},
+	{
+		symbol: '5',
+		shift: '%',
+	},
+	{
+		symbol: '6',
+		symbolSm: ':',
+		shift: '^',
+	},
+	{
+		symbol: '7',
+		symbolSm: '?',
+		shift: '&',
+	},
+	{
+		symbol: '8',
+		shift: '*',
+	},
+	{
+		symbol: '9',
+		shift: '(',
+	},
+	{
+		symbol: '0',
+		symbolSm: '%',
+		shift: ')',
+	},
+	{
+		symbol: '-',
+		shift: '_',
+	},
+	{
+		symbol: '=',
+		shift: '+',
+	},
+	{
+		symbol: 'keyboard_backspace',
+		shift: '',
+		hasIcon: true,
+		keyName: 'backspace',
+	},
+
+	{
+		symbol: '',
+		shift: '',
+		isBreak: true,
+	},
+
+	{
+		symbol: 'keyboard_tab',
+		shift: '',
+		hasIcon: true,
+		keyName: 'tab',
+	},
+	{
+		symbol: 'q',
+		symbolSm: 'Й',
+		shift: '',
+	},
+	{
+		symbol: 'w',
+		symbolSm: 'Ц',
+		shift: '',
+	},
+	{
+		symbol: 'e',
+		symbolSm: 'У',
+		shift: '',
+	},
+	{
+		symbol: 'r',
+		symbolSm: 'k',
+		shift: '',
+	},
+	{
+		symbol: 't',
+		symbolSm: 'Е',
+		shift: '',
+	},
+	{
+		symbol: 'y',
+		symbolSm: 'Н',
+		shift: '',
+	},
+	{
+		symbol: 'u',
+		symbolSm: 'Г',
+		shift: '',
+	},
+	{
+		symbol: 'i',
+		symbolSm: 'ш',
+		shift: '',
+	},
+	{
+		symbol: 'o',
+		symbolSm: 'Щ',
+		shift: '',
+	},
+	{
+		symbol: 'p',
+		symbolSm: 'З',
+		shift: '',
+	},
+	{
+		symbol: '[',
+		symbolSm: 'Х',
+		shift: '{',
+	},
+	{
+		symbol: ']',
+		symbolSm: 'Ъ',
+		shift: '}',
+	},
+	{
+		symbol: '\\',
+		symbolSm: '/',
+		shift: '|',
+	},
+	{
+		symbol: '',
+		shift: '',
+		isBreak: true,
+	},
+	{
+		symbol: 'keyboard_capslock',
+		shift: '',
+		hasIcon: true,
+		keyName: 'capslock',
+	},
+	{
+		symbol: 'a',
+		symbolSm: 'Ф',
+		shift: '',
+	},
+	{
+		symbol: 's',
+		symbolSm: 'Ы',
+		shift: '',
+	},
+
+	{
+		symbol: 'd',
+		symbolSm: 'В',
+		shift: '',
+	},
+	{
+		symbol: 'f',
+		symbolSm: 'А',
+		shift: '',
+	},
+	{
+		symbol: 'g',
+		symbolSm: 'П',
+		shift: '',
+	},
+	{
+		symbol: 'h',
+		symbolSm: 'Р',
+		shift: '',
+	},
+	{
+		symbol: 'j',
+		symbolSm: 'О',
+		shift: '',
+	},
+	{
+		symbol: 'k',
+		symbolSm: 'Л',
+		shift: '',
+	},
+	{
+		symbol: 'l',
+		symbolSm: 'Д',
+		shift: '',
+	},
+	{
+		symbol: ';',
+		symbolSm: 'Ж',
+		shift: ':',
+	},
+	{
+		symbol: "'",
+		symbolSm: 'Э',
+		shift: '"',
+	},
+	{
+		symbol: 'keyboard_return',
+		shift: '',
+		hasIcon: true,
+		keyName: 'enter',
+	},
+	{
+		symbol: '',
+		shift: '',
+		isBreak: true,
+	},
+
+	{
+		symbol: 'shift',
+		shift: '',
+		keyName: 'shift',
+	},
+
+	{
+		symbol: 'z',
+		symbolSm: 'Я',
+		shift: '',
+	},
+
+	{
+		symbol: 'x',
+		symbolSm: 'Ч',
+		shift: '',
+	},
+	{
+		symbol: 'c',
+		symbolSm: 'с',
+		shift: '',
+	},
+	{
+		symbol: 'v',
+		symbolSm: 'М',
+		shift: '',
+	},
+	{
+		symbol: 'b',
+		symbolSm: 'И',
+		shift: '',
+	},
+	{
+		symbol: 'n',
+		symbolSm: 'т',
+		shift: '',
+	},
+	{
+		symbol: 'm',
+		symbolSm: 'ь',
+		shift: '',
+	},
+	{
+		symbol: ',',
+		symbolSm: 'Б',
+		shift: '<',
+	},
+	{
+		symbol: '.',
+		symbolSm: 'Ю',
+		shift: '>',
+	},
+	{
+		symbol: '/',
+		symbolSm: '.,',
+		shift: '?',
+	},
+
+	{
+		symbol: 'shift',
+		shift: '',
+		keyName: 'shift',
+	},
+	{
+		symbol: '',
+		shift: '',
+		isBreak: true,
+	},
+	{
+		symbol: 'keyboard_hide',
+		shift: '',
+		hasIcon: true,
+		keyName: 'hide',
+	},
+	{
+		symbol: 'en',
+		shift: '',
+		keyName: 'lang',
+	},
+	{
+		symbol: 'mic',
+		shift: '',
+		hasIcon: true,
+		keyName: 'microphone',
+	},
+	{
+		symbol: '_______ _______',
+		shift: '',
+		keyName: 'space',
+	},
+	{
+		symbol: 'volume_up',
+		shift: '',
+		hasIcon: true,
+		keyName: 'volume_up',
+	},
+	{
+		symbol: 'keyboard_arrow_left',
+		shift: '',
+		hasIcon: true,
+		keyName: 'moveleft',
+	},
+	{
+		symbol: 'keyboard_arrow_right',
+		shift: '',
+		hasIcon: true,
+		keyName: 'moveright',
+	},
+];
---
+++ virtual-keyboard/data-lang/ru.js
@@ -0,0 +1,332 @@
+const ru = [
+	{
+		symbol: 'Ё',
+		symbolSm: '`',
+		shift: '',
+	},
+	{
+		symbol: '1',
+		shift: '!',
+	},
+	{
+		symbol: '2',
+		symbolSm: '@',
+		shift: '"',
+	},
+	{
+		symbol: '3',
+		symbolSm: '#',
+		shift: '№',
+	},
+	{
+		symbol: '4',
+		shift: ';',
+		symbolSm: '$',
+	},
+	{
+		symbol: '5',
+		shift: '%',
+	},
+	{
+		symbol: '6',
+		symbolSm: '^',
+		shift: ':',
+	},
+	{
+		symbol: '7',
+		symbolSm: '&',
+		shift: '?',
+	},
+	{
+		symbol: '8',
+		shift: '*',
+	},
+	{
+		symbol: '9',
+		shift: '(',
+	},
+	{
+		symbol: '0',
+		symbolSm: '%',
+		shift: ')',
+	},
+	{
+		symbol: '-',
+		shift: '_',
+	},
+	{
+		symbol: '=',
+		shift: '+',
+	},
+	{
+		symbol: 'keyboard_backspace',
+		shift: '',
+		hasIcon: true,
+		keyName: 'backspace',
+	},
+
+	{
+		symbol: '',
+		shift: '',
+		isBreak: true,
+	},
+
+	{
+		symbol: 'keyboard_tab',
+		shift: '',
+		hasIcon: true,
+		keyName: 'tab',
+	},
+	{
+		symbol: 'Й',
+		symbolSm: 'q',
+		shift: '',
+	},
+	{
+		symbol: 'Ц',
+		symbolSm: 'w',
+		shift: '',
+	},
+	{
+		symbol: 'У',
+		symbolSm: 'e',
+		shift: '',
+	},
+	{
+		symbol: 'k',
+		symbolSm: 'r',
+		shift: '',
+	},
+	{
+		symbol: 'Е',
+		symbolSm: 't',
+		shift: '',
+	},
+	{
+		symbol: 'Н',
+		symbolSm: 'y',
+		shift: '',
+	},
+	{
+		symbol: 'Г',
+		symbolSm: 'u',
+		shift: '',
+	},
+	{
+		symbol: 'ш',
+		symbolSm: 'i',
+		shift: '',
+	},
+	{
+		symbol: 'Щ',
+		symbolSm: 'o',
+		shift: '',
+	},
+	{
+		symbol: 'З',
+		symbolSm: 'p',
+		shift: '',
+	},
+	{
+		symbol: 'Х',
+		symbolSm: '[',
+		shift: '',
+	},
+	{
+		symbol: ']',
+		symbolSm: 'Ъ',
+		shift: '',
+	},
+	{
+		symbol: '/',
+		symbolSm: '\\',
+		shift: '\\',
+	},
+	{
+		symbol: '',
+		shift: '',
+		isBreak: true,
+	},
+	{
+		symbol: 'keyboard_capslock',
+		shift: '',
+		hasIcon: true,
+		keyName: 'capslock',
+	},
+	{
+		symbol: 'Ф',
+		symbolSm: 'a',
+		shift: '',
+	},
+	{
+		symbol: 'Ы',
+		symbolSm: 's',
+		shift: '',
+	},
+
+	{
+		symbol: 'В',
+		symbolSm: 'd',
+		shift: '',
+	},
+	{
+		symbol: 'А',
+		symbolSm: 'f',
+		shift: '',
+	},
+	{
+		symbol: 'П',
+		symbolSm: 'g',
+		shift: '',
+	},
+	{
+		symbol: 'Р',
+		symbolSm: 'h',
+		shift: '',
+	},
+	{
+		symbol: 'О',
+		symbolSm: 'j',
+		shift: '',
+	},
+	{
+		symbol: 'Л',
+		symbolSm: 'k',
+		shift: '',
+	},
+	{
+		symbol: 'Д',
+		symbolSm: 'l',
+		shift: '',
+	},
+	{
+		symbol: 'Ж',
+		symbolSm: ';',
+		shift: '',
+	},
+	{
+		symbol: 'Э',
+		symbolSm: "'",
+		shift: '',
+	},
+	{
+		symbol: 'keyboard_return',
+		shift: '',
+		hasIcon: true,
+		keyName: 'enter',
+	},
+	{
+		symbol: '',
+		shift: '',
+		isBreak: true,
+	},
+
+	{
+		symbol: 'shift',
+		shift: '',
+		keyName: 'shift',
+	},
+
+	{
+		symbol: 'Я',
+		symbolSm: 'z',
+		shift: '',
+	},
+
+	{
+		symbol: 'Ч',
+		symbolSm: 'x',
+		shift: '',
+	},
+	{
+		symbol: 'с',
+		symbolSm: 'c',
+		shift: '',
+	},
+	{
+		symbol: 'М',
+		symbolSm: 'v',
+		shift: '',
+	},
+	{
+		symbol: 'И',
+		symbolSm: 'b',
+		shift: '',
+	},
+	{
+		symbol: 'т',
+		symbolSm: 'n',
+		shift: '',
+	},
+	{
+		symbol: 'ь',
+		symbolSm: 'm',
+		shift: '',
+	},
+	{
+		symbol: 'Б',
+		symbolSm: ',',
+		shift: '',
+	},
+	{
+		symbol: 'Ю',
+		symbolSm: '.',
+		shift: '',
+	},
+	{
+		symbol: '.',
+		symbolSm: '/',
+		shift: ',',
+	},
+
+	{
+		symbol: 'shift',
+		shift: '',
+		keyName: 'shift',
+	},
+	{
+		symbol: '',
+		shift: '',
+		isBreak: true,
+	},
+	{
+		symbol: 'keyboard_hide',
+		shift: '',
+		hasIcon: true,
+		keyName: 'hide',
+	},
+	{
+		symbol: 'en',
+		shift: '',
+		keyName: 'lang',
+	},
+	{
+		symbol: 'mic',
+		shift: '',
+		hasIcon: true,
+		keyName: 'microphone',
+	},
+	{
+		symbol: '_______ _______',
+		shift: '',
+		keyName: 'space',
+	},
+	{
+		symbol: 'volume_up',
+		shift: '',
+		hasIcon: true,
+		keyName: 'volume_up',
+	},
+	{
+		symbol: 'keyboard_arrow_left',
+		shift: '',
+		hasIcon: true,
+		keyName: 'moveleft',
+	},
+	{
+		symbol: 'keyboard_arrow_right',
+		shift: '',
+		hasIcon: true,
+		keyName: 'moveright',
+	},
+];
