5d86c827302fe7a78258da31e0139fba41fade72
Gunel Huseynova <gorellik246@gmail.com>
2020-11-26 23:40:19 +0300

    refactor: refactor package.json file

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,5 +1,6 @@
 const { merge } = require('webpack-merge');
 const common = require('./webpack.common.js');
+const path = require('path');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = merge(common, {
